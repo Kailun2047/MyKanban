ef4ea8e985fb89604f6ee058f8304bad0682ae09
Kailun2047 <719477648@qq.com>
2019-10-15 23:56:37 -0400

    initialize redux store

--- ppm-react-client/src/App.js
+++ ppm-react-client/src/App.js
@@ -5,17 +5,21 @@ import Header from "./Layouts/Header";
 import Dashboard from "./components/Dashboard";
 import AddProject from "./components/Project/AddProject";
 import { BrowserRouter, Route } from "react-router-dom";
+import { Provider } from "react-redux";
+import store from "./store";
 
 class App extends Component {
   render() {
     return (
-      <BrowserRouter>
-        <div className="App">
-          <Header />
-          <Route exact path="/dashboard" component={Dashboard} />
-          <Route exact path="/addProject" component={AddProject} />
-        </div>
-      </BrowserRouter>
+      <Provider store={store}>
+        <BrowserRouter>
+          <div className="App">
+            <Header />
+            <Route exact path="/dashboard" component={Dashboard} />
+            <Route exact path="/addProject" component={AddProject} />
+          </div>
+        </BrowserRouter>
+      </Provider>
     );
   }
 }
